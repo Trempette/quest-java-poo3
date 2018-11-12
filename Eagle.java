53970a7fea107bd80f4704ed7878e1858fce03b4
Edouard Kerhir <edouard.kerhir@gmail.com>
2018-11-12 19:59:27 +0100

    interface Fly added

--- Eagle.java
+++ Eagle.java
@@ -1,4 +1,4 @@
-public class Eagle extends Bird {
+public class Eagle extends Bird implements Fly {
 
     private boolean flying;
     private int altitude;
@@ -21,4 +21,52 @@ public class Eagle extends Bird {
     public String sing() {
         return "Screech!";
     }
+
+    
+    public void takeOff(){
+        if(!this.isFlying() && altitude == 0){
+            this.flying = true;
+            System.out.printf("%s flies into the beautiful and endless sky. %n", this.getName());
+        }
+
+    } 
+
+    
+    public int ascend(int meters){
+        if(this.isFlying()){
+            this.altitude+=meters;
+            System.out.printf("%s flies upward. Altitude : %d %n", this.getName(), this.altitude);
+        }
+        else{
+            System.out.printf("%s is not flying. Dumbass. Altitude : %d", this.getName(), this.altitude);    
+        }
+        return this.altitude;
+
+    } 
+
+    
+    public int descend(int meters){
+        if(this.isFlying()){
+            this.altitude-=meters;
+            System.out.printf("%s flies downward. Altitude : %d %n", this.getName(), this.altitude);
+        }
+        else{
+            System.out.printf("%s is not flying. Dumbass. Altitude : %d", this.getName(), this.altitude);    
+        }
+        return this.altitude;
+    }
+    
+    
+    public void land(){
+        if (this.flying && this.altitude == 0) {
+            System.out.printf("%s gets on the ground.%n", this.getName());
+        } else {
+            System.out.printf("%s is too high !.%n", this.getName());
+        }
+
+    }
+
+    public void glide(){}
+
+
 }
